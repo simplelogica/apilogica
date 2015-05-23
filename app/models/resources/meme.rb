fa03a177bcf07493eb065d880fb98138ec715732
Enol Iglesias <enoliglesias@gmail.com>
2015-05-23 20:03:00 +0200

    Add meme resource skeleton

---
+++ app/models/resources/meme.rb
@@ -0,0 +1,17 @@
+module Resources
+  class Meme < Base
+    # URL for memecaptain
+    # http://v1.memecaptain.com/
+    def resource_url(query)
+    end
+
+    def request(query)
+    end
+
+    def get_name_photo_from_query(query)
+    end
+
+    def get_text_from_query(query)
+    end
+  end
+end
