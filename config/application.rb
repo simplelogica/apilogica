7718e996d7aa520fb78f0ce2eceffa865b9d2ef0
Ruben Sierra <maguilag@gmail.com>
2015-05-23 18:23:24 +0200

    Config for multiple locale files

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,9 @@ module Apilogica
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
