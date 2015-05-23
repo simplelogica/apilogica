00ccbcfc1869048eef1090e11195cb5315ade89c
Ruben Sierra <maguilag@gmail.com>
2015-05-23 18:46:51 +0200

    Add ‘en’ as available locale

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Apilogica
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-    config.i18n.available_locales = [:es]
+    config.i18n.available_locales = [:es, :en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
