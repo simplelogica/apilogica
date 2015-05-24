0e2d67c9a4cb1566338a787bad7ca9589db9ee19
Enol Iglesias <enoliglesias@gmail.com>
2015-05-24 02:26:13 +0200

    Add functionality to get the memecaptain url

--- lib/resources/meme.rb
+++ lib/resources/meme.rb
@@ -3,21 +3,21 @@ module Resources
     # URL for memecaptain
     # http://v1.memecaptain.com/
     def resource_url(query)
-      name_photo = self.get_name_photo_from_query(query)
-      up_text, down_text = self.get_text_from_query(query)
-      "#{@api_resource.endpoint}g?u=http%3A%2F%2Fv1.memecaptain.com%2F#{photo}&t1=#{up_text}&t2=#{down_text}"
+      matches = query.match(/meme (\S+)\s*\|\s*(.+)\s*\|\s*(.+)/)
+      image_name = YAML.load(File.open(File.join(File.dirname(__FILE__), "../assets/meme/meme_image_names.yml")))[matches[1]]
+      "#{@api_resource.endpoint}/"\
+      "g?u=http%3A%2F%2Fv1.memecaptain.com%2F"\
+      "#{image_name}"\
+      "&t1=#{matches[2]}"\
+      "&t2=#{matches[3]}"
+    rescue
+      "http://v1.memecaptain.com/g?u=http%3A%2F%2Fv1.memecaptain.com%2Ffirst_world_problems.jpg&t1=meme+404+error&t2=no+encuentro+lo+que+pides+%3A%27("
     end
 
     def request(query)
       url = self.resource_url(query)
       response = http_request(url)
       response['imageUrl']
-    end
-
-    def get_name_photo_from_query(query)
-    end
-
-    def get_text_from_query(query)
     end
   end
 end
