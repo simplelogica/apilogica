70b88079e1314bd92acf0192368628f2a508bad2
Enol Iglesias <enoliglesias@gmail.com>
2015-05-24 20:23:10 +0200

    Add custom memes functionality
    Added custom memes functionality, url generate depends
    on the meme, refactor, etc

--- lib/resources/meme.rb
+++ lib/resources/meme.rb
@@ -2,30 +2,55 @@ module Resources
   class Meme < Base
     # Freeze memes hash on load
     AVAILABLE_MEMES = YAML.load(File.open(File.join(File.dirname(__FILE__), "../assets/meme/meme_image_names.yml"))).freeze
-
-    def help
-      "#{@api_resource.name} 'meme name' | 'top text' | 'bottom text'. " \
-      "Available memes: #{AVAILABLE_MEMES.keys.join(', ')}"
-    end
-
-    # URL for memecaptain
-    # http://v1.memecaptain.com/
-    def resource_url(query)
-      matches = query.match(/(\S+)\s*\|\s*(.+)\s*\|\s*(.+)/)
-      image_name = AVAILABLE_MEMES[matches[1]]
-      "#{@api_resource.endpoint}/"\
-      "g?u=http%3A%2F%2Fv1.memecaptain.com%2F"\
-      "#{image_name}"\
-      "&t1=#{matches[2] rescue ''}"\
-      "&t2=#{matches[3] rescue ''}"
-    rescue
-      "http://v1.memecaptain.com/g?u=http%3A%2F%2Fv1.memecaptain.com%2Ffirst_world_problems.jpg&t1=meme+404+error&t2=no+encuentro+lo+que+pides+%3A%27("
-    end
+    CUSTOM_MEMES = YAML.load(File.open(File.join(File.dirname(__FILE__), "../assets/meme/custom_image_names.yml"))).freeze
 
     def request(query)
       url = self.resource_url(query)
       response = http_request(url)
       {image: response['imageUrl']}
     end
+
+    def help
+      "#{@api_resource.name} 'meme name' | 'top text' | 'bottom text'. " \
+      "Available memes: #{AVAILABLE_MEMES.merge(CUSTOM_MEMES).keys.join(', ')}"
+    end
+
+    # URL for memecaptain
+    # http://v1.memecaptain.com/
+    def resource_url(query)
+      self.generate_url(query)
+    rescue
+      self.help
+    end
+
+    def extract_matches_from_query(query)
+      #1: meme_name; 2: up_text; 3: down_text
+      matches = query.match(/(\S+)\s*\|\s*(.+)\s*\|\s*(.+)/)
+      [matches[1], (matches[2] rescue ""), (matches[3] rescue "")]
+    end
+
+    def generate_url(query)
+      meme_name, up_text, down_text = self.extract_matches_from_query(query)
+      if AVAILABLE_MEMES.keys.include?(meme_name)
+        self.generate_regular_url(meme_name, up_text, down_text)
+      elsif CUSTOM_MEMES.keys.include?(meme_name)
+        self.generate_custom_url(meme_name, up_text, down_text)
+      else
+        raise StandardError.new
+      end
+    end
+
+    def generate_custom_url(meme_name, up_text, down_text)
+      "http://v1.memecaptain.com/g?u=#{CUSTOM_MEMES[meme_name]}&t1=#{up_text}&t2=#{down_text}"
+    end
+
+    def generate_regular_url(meme_name, up_text, down_text)
+      "#{@api_resource.endpoint}/"\
+      "g?u=http%3A%2F%2Fv1.memecaptain.com%2F"\
+      "#{AVAILABLE_MEMES[meme_name]}"\
+      "&t1=#{up_text}"\
+      "&t2=#{down_text}"
+    end
+
   end
 end
